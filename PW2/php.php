89ad0ba945aa696c0992826d380feca3c0a57870
OWNER <heyniko911@gmail.com>
2023-02-27 13:52:04 +0700

    first commit

---
+++ PW2/php.php
@@ -0,0 +1,20 @@
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Belajar Bahasa PHP</title>
+    </head>
+    <body>
+    <?php
+    echo " <h1 style = color: conic-gradient(
+        #fd004c,#fe9000,#fff020,#3eff4b,
+        #3363ff,#b102b7,#fd004c
+    );> Hello World </h1>";
+    ?>
+    <hr>
+<h2 style="c" >Ini Title Tanpa Tag PHP</h2>
+</hr>
+    </body>
+    </html>
---
+++ PW2/php2.php
@@ -0,0 +1,21 @@
+<?php
+$nama_depan = "Nur";
+$namaDepan = "Nur";
+$namaBelakang = "Rachmat";
+$umur = 17;
+$ipk = 3.91;
+$hadir = false;
+$sallary = null;
+echo "Nama Lengkap = " . $nama_depan ." ". $namaBelakang ."<br>";
+echo "Nama Lengkap = $nama_depan $namaBelakang <br>";
+echo "Nama Lengkap = ${namaDepan} ${namaBelakang} <br>";
+echo "Umur = $umur Tahun" . "<br>";
+define("TEMPATLAHIR" , "Palembang");
+echo "Tempat Lahir = " . TEMPATLAHIR;
+$_SESSION['nama']
+$_COOKIE['namacookie']
+$_POST['npm']
+$_GET['npmget']
+$_SERVER['server']
+
+?>
